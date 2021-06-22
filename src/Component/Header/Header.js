ae6d6b91ad793c01306d7997a7253e6660739452
unknown <chetan.udemy@gmail.com>
2021-06-22 23:55:34 +0530

    Expense UI is completed

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -4,15 +4,9 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import { AuthContext } from '../../Store/Auth';
 import { NavLink } from 'react-router-dom';
@@ -122,7 +116,7 @@ export default function Header() {
       </MenuItem>
       <MenuItem>
         <NavLink to='/expense' className={classes.menuItem}>
-          expense
+          Expense
         </NavLink>
       </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
--- src/Pages/Expense/Expense.js
+++ src/Pages/Expense/Expense.js
@@ -4,6 +4,9 @@ import TableGD from '../../Component/TableGD/TableGD';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
+import TextField from '@material-ui/core/TextField';
+import SaveIcon from '@material-ui/icons/Save';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,6 +16,17 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(0, 3, 0, 3),
+  },
+  form: {
+    padding: theme.spacing(1),
+    width: '100%',
+    margin: 'auto',
+  },
+  grid: {
+    width: '100%',
+  },
+  p: {
+    padding: theme.spacing(1),
   },
 }));
 
@@ -25,12 +39,70 @@ const Expense = () => {
           <Grid item md={4} className={classes.paper}>
             <Paper>
               <Typography
-                variant='h6'
-                color='inherit'
-                noWrap
-                className={classes.toolbarTitle}>
-                sssssssssss
+                component='h1'
+                variant='h5'
+                align='center'
+                className={classes.p}>
+                Add Expense
               </Typography>
+              <form className={classes.form} noValidate autoComplete='off'>
+                <Grid container spacing={2}>
+                  <Grid item md={12}>
+                    <TextField
+                      autoComplete='fname'
+                      name='firstName'
+                      variant='outlined'
+                      required
+                      fullWidth
+                      label='First Name'
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item md={12}>
+                    <TextField
+                      autoComplete='fname'
+                      name='firstName'
+                      variant='outlined'
+                      required
+                      fullWidth
+                      label='First Name'
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item md={12}>
+                    <TextField
+                      autoComplete='fname'
+                      name='firstName'
+                      variant='outlined'
+                      required
+                      fullWidth
+                      label='First Name'
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item md={12}>
+                    <TextField
+                      autoComplete='fname'
+                      name='firstName'
+                      variant='outlined'
+                      required
+                      fullWidth
+                      label='First Name'
+                      autoFocus
+                    />
+                  </Grid>{' '}
+                  <Grid item md={12}>
+                    <Button
+                      variant='contained'
+                      color='primary'
+                      fullWidth
+                      className={classes.button}
+                      startIcon={<SaveIcon />}>
+                      Save
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
             </Paper>
           </Grid>
           <Grid item md={8}>
