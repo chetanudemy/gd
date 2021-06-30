49c45d72900565d49c23cc140297ad9c62e23e23
unknown <chetan.udemy@gmail.com>
2021-06-30 20:50:30 +0530

    B

--- src/Component/Skeleton/SkeletonProduct.js
+++ src/Component/Skeleton/SkeletonProduct.js
@@ -1,13 +1,33 @@
 import { Grid } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 
-const SkeletonProducts = () => {
+const SkeletonProducts = (props) => {
+  let width = 150;
+  if (isWidthUp('sm', props.width)) {
+    width = 210;
+  }
+
   return (
     <Grid container spacing={1}>
       {Array(1, 2, 3, 4).map((item) => {
         return (
           <Grid item md={3} xs={6} style={{ marginTop: 8 }} key={item}>
-            <Skeleton variant='rect' width={210} height={110} />
+            <Skeleton variant='rect' width={width} height={110} />
+          </Grid>
+        );
+      })}
+      {Array(1, 2, 3, 4).map((item) => {
+        return (
+          <Grid item md={3} xs={6} style={{ marginTop: 8 }} key={item}>
+            <Skeleton variant='rect' width={width} height={110} />
+          </Grid>
+        );
+      })}
+      {Array(1, 2, 3, 4).map((item) => {
+        return (
+          <Grid item md={3} xs={6} style={{ marginTop: 8 }} key={item}>
+            <Skeleton variant='rect' width={width} height={110} />
           </Grid>
         );
       })}
@@ -15,4 +35,4 @@ const SkeletonProducts = () => {
   );
 };
 
-export default SkeletonProducts;
+export default withWidth()(SkeletonProducts);
